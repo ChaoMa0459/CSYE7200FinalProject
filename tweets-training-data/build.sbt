af769317561475f235fb8afcf1b80142a87eb641
Yan <sun.yan3@husky.neu.edu>
2020-04-08 16:46:48 -0400

    Separate sentence in text column into words in df_train and df_test

--- tweets-training-data/build.sbt
+++ tweets-training-data/build.sbt
@@ -15,3 +15,12 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-sql" % "2.4.5"
 )
 
+name := "stop-word-remover"
+organization := "com.ashrithgn.scala.tut"
+version := "1.0"
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.2.3",
+  "org.apache.spark" %% "spark-mllib" % "2.4.4" % "compile"
+)
--- tweets-training-data/src/main/scala/classification/models/Random_Forest.scala
+++ tweets-training-data/src/main/scala/classification/models/Random_Forest.scala
@@ -2,6 +2,10 @@ package classification.models
 
 import org.apache.spark.sql.types.{StructType,StructField,StringType,IntegerType};
 import org.apache.spark.sql.Row;
+import org.apache.spark.ml.feature.StopWordsRemover
+import org.apache.spark.ml.feature.{RegexTokenizer, Tokenizer}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 class Random_Forest {
   val spark = org.apache.spark.sql.SparkSession.builder
@@ -20,4 +24,21 @@ class Random_Forest {
     .load("src/test/resources/test.csv")
   df_train.show()
   df_test.show()
+//Separate sentence in text column into words in df_train and df_test
+  val tokenizer = new Tokenizer().setInputCol("text").setOutputCol("words")
+  val regexTokenizer = new RegexTokenizer()
+    .setInputCol("text")
+    .setOutputCol("words")
+    .setPattern("\\W") // alternatively .setPattern("\\w+").setGaps(false)
+
+  val countTokens = udf { (words: Seq[String]) => words.length }
+
+  val tokenized = tokenizer.transform(df_train)
+  tokenized.select("text", "words")
+    .withColumn("tokens", countTokens(col("words"))).show(false)
+
+  val regexTokenized = regexTokenizer.transform(df_train)
+  regexTokenized.select("text", "words")
+    .withColumn("tokens", countTokens(col("words"))).show(false)
+
 }
