69ce326f33eb7ed1a35cfefe80fdd42887c4db62
Chao Ma <ma.cha@northeastern.edu>
2020-04-16 01:22:12 -0400

    added unit tests

--- tweets-training-data/src/test/scala/edu/neu/coe/csye7200/wordcount/SparkWordCountSpec.scala
+++ tweets-training-data/src/test/scala/edu/neu/coe/csye7200/wordcount/SparkWordCountSpec.scala
@@ -1,7 +1,8 @@
 package edu.neu.coe.csye7200.wordcount
 
-// import edu.neu.coe.csye7200.wordcount.SparkWordCount.get_word_frequencies
+import edu.neu.coe.csye7200.wordcount.SparkWordCount.filterData
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Dataset, Row}
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.io.Source
@@ -9,12 +10,25 @@ import scala.io.Source
 class SparkWordCountSpec extends FlatSpec with Matchers {
   behavior of "word frequencies"
 
-//  it should "work for real disaster tweets" in {
-//    val real_words_counts = get_word_frequencies()._1
-//    val size:Long = real_words_counts match {
-//      case null => 0
-//      case w: RDD[(String, Int)] => w.count()
-//    }
-//    assert(size > 0)
-//  }
+  val filteredData: (Dataset[Row], Dataset[Row]) = filterData()
+
+  it should "work for real disaster tweets count" in {
+    val real_words = filteredData._1
+    assert(real_words.count() == 3081)
+  }
+
+  it should "work for fake disaster tweets count" in {
+    val fake_words = filteredData._2
+    assert(fake_words.count() == 4095)
+  }
+
+  it should "work for real disaster tweets target" in {
+    val target: String = filteredData._1.collect()(0).getAs("target")
+    assert(target == "1")
+  }
+
+  it should "work for fake disaster tweets target" in {
+    val target: String = filteredData._2.collect()(0).getAs("target")
+    assert(target == "0")
+  }
 }
